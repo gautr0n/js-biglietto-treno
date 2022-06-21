9f2b4ce67557ea979a18c07160a92961fd5c42ce
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2022-06-21 15:55:19 +0200

    result

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@
 //va applicato uno sconto del 40% per gli over 65
 //stampa il risultato con massimo 2 cifre decimali
 
+const formatter = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
+
 let km = parseInt(prompt('inserire i km da percorrere', '6'));
 let age = parseInt(prompt('inserire la propria età', '21'));
 
@@ -17,3 +22,6 @@ if(age<18){
 }
 console.log(cost);
 
+const resultCost = document.getElementById('result');
+
+resultCost.innerText = `Il costo del treno è ${cost}`;
