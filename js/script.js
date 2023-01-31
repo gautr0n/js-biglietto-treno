1a6bc6bba9be294c7fa51e61779b3f94aac9e6bf
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-01-31 16:00:43 +0100

    richiesta informazioni e calcolo costo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 //1 chiedere informazioni all'utente
+const personalAge = prompt('inserisci la tua età');
+const totalKilometers = prompt('inserisci la quantita di km che intendi percorrere');
+console.log(personalAge);
+console.log(totalKilometers);
+
 //2 calcolare costo
+const ticketCost = totalKilometers * 0.21;
+console.log(ticketCost);
+
 //3 calcolare sconti
+
+
+
 //4 emettere il risultato
